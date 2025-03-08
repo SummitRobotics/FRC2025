f35b2a781b40856e900b4ec736d97e5dd8b048e9
John Folliard <johnfolliard3@yahoo.com>
2025-03-07 22:38:58 -0800

    perform signed clamping of velocity and constraints

--- src/main/java/frc/robot/commands/PIDtoPose.java
+++ src/main/java/frc/robot/commands/PIDtoPose.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.utilities.Functions;
 
 // TODO - move to inside of CommandSwerveDrivetrain subsystem for encapsulation purposes
 public class PIDtoPose extends Command {
@@ -26,9 +27,9 @@ public class PIDtoPose extends Command {
         yController = new PIDController(4, 0, 0.1);
         rotateController = new PIDController(0.2, 0, 0);
         request = new SwerveRequest.FieldCentric()
-            .withDeadband(0).withRotationalDeadband(0)
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage) // Use open-loop control for drive motors
-            .withSteerRequestType(SteerRequestType.MotionMagicExpo);
+                .withDeadband(0).withRotationalDeadband(0)
+                .withDriveRequestType(DriveRequestType.OpenLoopVoltage) // Use open-loop control for drive motors
+                .withSteerRequestType(SteerRequestType.MotionMagicExpo);
         // rotateController.setTolerance(50);
         // xController.setTolerance(1);
         // yController.setTolerance(1);
@@ -40,10 +41,14 @@ public class PIDtoPose extends Command {
     @Override
     public void execute() {
         drivetrain.setControl(request
-            .withVelocityX(Math.min(maxVel, xController.calculate(drivetrain.getState().Pose.getX() - setpoint.getX())))
-            .withVelocityY(Math.min(maxVel, yController.calculate(drivetrain.getState().Pose.getY() - setpoint.getY())))
-            .withRotationalRate(rotateController.calculate(drivetrain.getState().Pose.getRotation().minus(setpoint.getRotation()).getDegrees()))
-        );
+                .withVelocityX(Functions.clampMagnitude(
+                        xController.calculate(drivetrain.getState().Pose.getX() - setpoint.getX()),
+                        maxVel))
+                .withVelocityY(Functions.clampMagnitude(
+                        yController.calculate(drivetrain.getState().Pose.getY() - setpoint.getY()),
+                        maxVel))
+                .withRotationalRate(rotateController.calculate(
+                        drivetrain.getState().Pose.getRotation().minus(setpoint.getRotation()).getDegrees())));
         System.out.println("X: " + xAtSetpoint());
         System.out.println("Y: " + yAtSetpoint());
         System.out.println("Rotate: " + rotateAtSetpoint());
@@ -52,15 +57,15 @@ public class PIDtoPose extends Command {
     }
 
     private boolean xAtSetpoint() {
-        return (drivetrain.getState().Pose.getX() - setpoint.getX()) < 0.02;
+        return Math.abs(drivetrain.getState().Pose.getX() - setpoint.getX()) < 0.02;
     }
 
     private boolean yAtSetpoint() {
-        return (drivetrain.getState().Pose.getY() - setpoint.getY()) < 0.02;
+        return Math.abs(drivetrain.getState().Pose.getY() - setpoint.getY()) < 0.02;
     }
 
     private boolean rotateAtSetpoint() {
-        return drivetrain.getState().Pose.getRotation().minus(setpoint.getRotation()).getDegrees() < 2.5;
+        return Math.abs(drivetrain.getState().Pose.getRotation().minus(setpoint.getRotation()).getDegrees()) < 2.5;
     }
 
     @Override
@@ -70,6 +75,8 @@ public class PIDtoPose extends Command {
 
     @Override
     public boolean isFinished() {
-        return xAtSetpoint() && yAtSetpoint() && rotateAtSetpoint() && drivetrain.getState().Speeds.vxMetersPerSecond < 0.1 && drivetrain.getState().Speeds.vyMetersPerSecond < 0.1;
+        return xAtSetpoint() && yAtSetpoint() && rotateAtSetpoint()
+                && Math.abs(drivetrain.getState().Speeds.vxMetersPerSecond) < 0.1
+                && Math.abs(drivetrain.getState().Speeds.vyMetersPerSecond) < 0.1;
     }
 }
