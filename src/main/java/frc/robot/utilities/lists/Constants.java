49f0efa91282181d6bbabe581a9c6dad42d32107
driverstation <>
2025-04-07 17:11:24 -0700

    Poat-DCMP

--- src/main/java/frc/robot/utilities/lists/Constants.java
+++ src/main/java/frc/robot/utilities/lists/Constants.java
@@ -107,7 +107,7 @@ public class Constants {
             kS = 0,
             kV = 1.2,
             kA = 0,
-            MAX_ROTATIONS = 210,
+            MAX_ROTATIONS = 150,
             BACK_ROTATIONS = -50.5,
             MAX_VELOCITY_IN = 180,
             MAX_ACCEL_IN = 200,
